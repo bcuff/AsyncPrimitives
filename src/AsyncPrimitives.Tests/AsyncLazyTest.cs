5798ade453d3296f152173d1c0d2346285a5022c
Brandon <brandoncuff@gmail.com>
2014-06-10 15:03:09 -0700

    Added AsyncLazy<T> class.

---
+++ src/AsyncPrimitives.Tests/AsyncLazyTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AsyncPrimitives.Tests
+{
+    [TestClass]
+    public class AsyncLazyTest
+    {
+        [TestMethod]
+        public async Task TestGetAwaiter()
+        {
+            var target = new AsyncLazy<int>(async () => { await Task.Delay(1); return 1; });
+            Assert.AreEqual(1, await target);
+        }
+    }
+}
---
+++ src/AsyncPrimitives/AsyncLazy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace AsyncPrimitives
+{
+    /// <summary>
+    /// Provides support for async lazy initialization.
+    /// </summary>
+    /// <typeparam name="T">The type to lazily create.</typeparam>
+    public class AsyncLazy<T> : Lazy<Task<T>>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AsyncLazy{T}"/> class.
+        /// </summary>
+        /// <param name="taskFactory">The delegate that is invoked to produce the lazily initialized <see cref="Task{T}"/> value when it is needed.</param>
+        public AsyncLazy(Func<Task<T>> taskFactory)
+            : base(taskFactory, true)
+        {
+        }
+
+        /// <summary>
+        /// Gets an awaiter used to await this <see cref="AsyncLazy{T}"/>.
+        /// </summary>
+        /// <returns>An awaiter instance.</returns>
+        /// <remarks>This method is intended for compiler user rather than use directly in code.</remarks>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public TaskAwaiter<T> GetAwaiter()
+        {
+            return Value.GetAwaiter();
+        }
+    }
+}
